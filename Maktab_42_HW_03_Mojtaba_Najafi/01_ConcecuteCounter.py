90941fe19de8cfd973a023ce8fe9ded040f06804
moshnar <moshnar@gmail.com>
2020-09-04 15:00:22 +0300

    new HW added

---
+++ Maktab_42_HW_03_Mojtaba_Najafi/01_ConcecuteCounter.py
@@ -0,0 +1,22 @@
+#s= input("Enter any String ...")
+s = '11221133444'
+count=1
+output=''
+outputFormat= '({},{})'
+if len(s)>1:
+    for i in range(1,len(s)):
+       if s[i-1]==s[i]:
+          count+=1
+       else :
+           output += outputFormat.format(count,s[i-1])
+           count=1
+    output += outputFormat.format(count,s[i])
+else:
+
+    output = "the string is empty"
+
+
+print (output)
+print(type(output))
+
+
