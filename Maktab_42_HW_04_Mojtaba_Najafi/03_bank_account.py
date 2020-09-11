fc24e9e83678b5018fce79bfdeb5066faea8382d
moshnar <moshnar@gmail.com>
2020-09-11 04:43:26 +0300

    minor changes

--- Maktab_42_HW_04_Mojtaba_Najafi/03_bank_account.py
+++ Maktab_42_HW_04_Mojtaba_Najafi/03_bank_account.py
@@ -6,11 +6,13 @@ class Account:
 
     def deposit(self,amount): # make a deposit
         self.balance += amount
+        print("Deposit was successful!")
 
     def withdraw(self,amount):# make a withdraw considering the minimum balance
         if self.__withdrawable(amount):
 
             self.balance -= amount
+            print("Withdraw was successful!")
         else :
             print(f'Maximum withdraw is {self.balance - self.min_balance}')
 
@@ -20,6 +22,7 @@ class Account:
 
             self.balance = self.balance - amount
             name.balance = name.balance + amount
+            print(f"transfer was successful and the new balance is {self.balance}")
         else:
             print(f'Maximum transfer amount is {self.balance - self.min_balance}')
 
@@ -41,5 +44,5 @@ a.transfer(500,b)
 
 print(a.balance)
 print(b.balance)
-
+a.transfer(50,b)
 a.transfer(500,b)
